1580529756b212822dc7abe615161f3253e6a86f
Mayank Jain <mayank007.j@gmail.com>
2016-06-17 10:43:32 +0530

    jade

--- view/page-1/js/script.js
+++ view/page-1/js/script.js
@@ -5,19 +5,6 @@ $('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
-$(document).ready(function () {
-    var carousel = $("#owl-demo");
-  carousel.owlCarousel({
-    navigation:true,
-    navigationText: [
-      "<i class='icon-chevron-left icon-white'><</i>",
-      "<i class='icon-chevron-right icon-white'>></i>"
-      ],
-  });
-
-  
-});
-
 
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
@@ -32,6 +19,4 @@ $(document).ready(function() {
   });
 });
 
-
-  
 
